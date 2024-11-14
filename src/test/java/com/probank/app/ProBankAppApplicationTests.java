4368a3ef0e47a305e56e5fcafec9e5e36bc95932
Israel Oyerinde <sogo.oyerinde@gmail.com>
2024-11-14 22:07:43 +0100

    first commit

---
+++ src/test/java/com/probank/app/ProBankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.probank.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProBankAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
