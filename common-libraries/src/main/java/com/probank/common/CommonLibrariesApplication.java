d4d1509646cd435c2c9bebb45ee7ebe8c574ca5e
Israel Oyerinde <sogo.oyerinde@gmail.com>
2024-11-16 10:33:04 +0100

    add dependencies in userservice and wallet service pom.xml file as well as common libraries module

---
+++ common-libraries/src/main/java/com/probank/common/CommonLibrariesApplication.java
@@ -0,0 +1,13 @@
+package com.probank.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonLibrariesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonLibrariesApplication.class, args);
+	}
+
+}
---
+++ common-libraries/src/test/java/com/probank/common/CommonLibrariesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.probank.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonLibrariesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
